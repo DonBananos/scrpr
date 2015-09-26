0b31b51aa9a94856f8c3b33c14a095b833645854
HLO <HLO@10.111.179.254>
2015-09-26 14:03:54 +0200

    user_model.php, database_controller.php

---
+++ app/controller/database_controller.php
@@ -0,0 +1,7 @@
+<?php
+
+class database_controller {
+    
+    private db_con = new mysqli('85.119.155.19', 'phplogin', 'Mowgli42', ' ');
+    
+}
---
+++ app/model/user_model.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * User Object
+ *
+ * @author HLO
+ */
+class User_model 
+{
+    function __construct() 
+    {
+        
+    }
+    
+    public function save_new_user(){
+        
+    }
+    
+    
+            
+}
